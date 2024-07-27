9a3191b3d5adf311736ef8634f61b23456b83bf0
Ian Flynn <himitsu.monban@gmail.com>
2024-07-27 00:45:46 -0400

    error handling and stuff

--- Program.cs
+++ Program.cs
@@ -9,7 +9,14 @@ class Program
         IPAddress localAddr = IPAddress.Parse("127.0.0.1");
         var endpoint = new IPEndPoint(localAddr, 13000);
         var s = new HTTP.Server(endpoint);
-        s.Start();
+        try
+        {
+            s.Start();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Something terrible has happened: {0}", e);
+        }
     }
 }
 
@@ -27,7 +34,7 @@ namespace HTTP
             try
             {
                 listener.Start();
-                Console.WriteLine($"Listening on {listener.LocalEndpoint}");
+                Console.WriteLine("Listening on {0}", listener.LocalEndpoint);
 
                 while (true)
                 {
@@ -35,28 +42,37 @@ namespace HTTP
                     Task.Run(() => ServeHTTP(client));
                 }
             }
-            catch (SocketException e)
+            catch
             {
-                Console.WriteLine("SocketException: {0}", e);
+                Console.WriteLine("unable to start server");
+                throw;
             }
         }
 
         public static void ServeHTTP(TcpClient client)
         {
-            // Wait for the client to finish sending its
-            // request (we don't care what it is)
-            var stream = client.GetStream();
-            byte[] buf = new byte[128];
-            int n = stream.Read(buf);
-            var req = Encoding.UTF8.GetString(buf);
+            try
+            {
+                // Wait for the client to finish sending its
+                // request (we don't care what it is)
+                var stream = client.GetStream();
+                Request req = new Request(stream);
+                Console.WriteLine(req);
 
-            var res = new HTTP.Response("Hello, world");
-            byte[] rBody = res.GetBytes();
-            stream.Write(rBody, 0, rBody.Length);
-
-            // Do I need to dispose the client, or does it happen
-            // automatically when it goes out scope?
-            client.Dispose();
+                var res = new HTTP.Response("Hello, world");
+                byte[] rBody = res.GetBytes();
+                stream.Write(rBody, 0, rBody.Length);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                // Do I need to dispose the client, or does it happen
+                // automatically when it goes out scope?
+                client.Dispose();
+            }
         }
         TcpListener listener;
     }
@@ -68,47 +84,55 @@ namespace HTTP
             uri = "";
             headers = new Dictionary<string, string> { };
         }
+
+        public Request(Stream s)
+        {
+            var reader = new StreamReader(s);
+
+            string? firstLine = reader.ReadLine();
+            if (firstLine is not string)
+            {
+                throw new Exception("unable to read from socket");
+            }
+            Console.WriteLine(firstLine);
+            string[] fields = firstLine.Split(' ');
+            if (fields.Length < 3)
+            {
+                throw new Exception($"invalid HTTP request: {firstLine}");
+            }
+            Console.WriteLine(fields);
+            if (Stuff.methodFromString(fields[0]) is Method meth)
+            {
+                method = meth;
+            }
+            uri = fields[1];
+
+            headers = new Dictionary<string, string> { };
+            string? line = reader.ReadLine();
+            while (line is string && line.Length > 0)
+            {
+                string[] l = line.Split(": ");
+                if (l.Length == 2)
+                {
+                    headers.Add(l[0], l[1]);
+                }
+                line = reader.ReadLine();
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder s = new StringBuilder();
+            s.AppendLine($"{method} {uri}");
+            foreach (var header in headers)
+            {
+                s.AppendLine(header.ToString());
+            }
+            return s.ToString();
+        }
         public Method method;
         public string uri;
         public Dictionary<string, string> headers;
-    }
-
-    class RequestBuilder
-    {
-        public void Write(byte[] foo)
-        {
-            foreach (byte b in foo)
-            {
-                this.Write(b);
-            }
-        }
-
-        public void Write(byte b)
-        {
-            switch (state)
-            {
-                case State.method:
-                    if (b == ' ')
-                    {
-                        state = State.uri;
-                    }
-                    else
-                    {
-                        request.method += b;
-                    }
-                    break;
-
-            }
-        }
-
-        Request request = new Request();
-        public bool isComplete;
-        State state = State.method;
-        enum State
-        {
-            method,
-            uri,
-        }
     }
 
     class Response
@@ -123,17 +147,18 @@ namespace HTTP
         public byte[] GetBytes()
         {
             var s = new StringBuilder();
-            Action<string> addline = (line) =>
+            // Use our own AppendLine to make sure we get CRLF
+            Action<string> AppendLine = (line) =>
             {
                 s.Append(line);
                 s.Append("\r\n");
             };
-            addline($"HTTP/1.1 {responseCode}");
-            addline("Content-Type: text/plain; charset=UTF-8");
-            addline($"Content-Length: {body.Length}");
-            addline("");
-            addline(body);
-            return Encoding.UTF8.GetBytes(s.ToString());
+            AppendLine($"HTTP/1.1 {responseCode}");
+            AppendLine("Content-Type: text/plain; charset=UTF-8");
+            AppendLine($"Content-Length: {body.Length}");
+            AppendLine("");
+            AppendLine(body);
+            return Encoding.ASCII.GetBytes(s.ToString());
         }
 
         int responseCode;
@@ -148,5 +173,28 @@ namespace HTTP
         PUT,
         PATCH,
         DELETE,
+    }
+
+    class Stuff // Come up with a better name later
+    {
+        public static Method? methodFromString(string s)
+        {
+            switch (s.ToUpper())
+            {
+                case "GET":
+                    return Method.GET;
+                case "POST":
+                    return Method.POST;
+                case "PUT":
+                    return Method.PUT;
+                case "PATCH":
+                    return Method.PATCH;
+                case "DELETE":
+                    return Method.DELETE;
+                default:
+                    return null;
+            }
+        }
+
     }
 }
