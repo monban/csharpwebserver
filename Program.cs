f671af9445978340f4bf53e3765a69dfda33bfba
Ian Flynn <himitsu.monban@gmail.com>
2024-07-26 21:28:45 -0400

    basic HTTP response

--- Program.cs
+++ Program.cs
@@ -1,60 +1,30 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 class MyTcpListener
 {
     public static void Main()
     {
-        TcpListener? server = null;
+        // Set the TcpListener on port 13000.
+        Int32 port = 13000;
+        IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+        var server = new TcpListener(localAddr, port);
+
         try
         {
-            // Set the TcpListener on port 13000.
-            Int32 port = 13000;
-            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-
-            // TcpListener server = new TcpListener(port);
-            server = new TcpListener(localAddr, port);
-
-            // Start listening for client requests.
             server.Start();
+            Console.WriteLine($"Listening on {localAddr}:{port}");
 
-            // Buffer for reading data
-            Byte[] bytes = new Byte[256];
-            String? data = null;
-
-            // Enter the listening loop.
             while (true)
             {
-                Console.Write("Waiting for a connection... ");
-
-                // Perform a blocking call to accept requests.
-                // You could also use server.AcceptSocket() here.
                 using TcpClient client = server.AcceptTcpClient();
                 Console.WriteLine("Connected!");
 
-                data = null;
-
-                // Get a stream object for reading and writing
                 NetworkStream stream = client.GetStream();
 
-                int i;
-
-                // Loop to receive all the data sent by the client.
-                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                {
-                    // Translate data bytes to a ASCII string.
-                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                    Console.WriteLine("Received: {0}", data);
-
-                    // Process the data sent by the client.
-                    data = data.ToUpper();
-
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                    // Send back a response.
-                    stream.Write(msg, 0, msg.Length);
-                    Console.WriteLine("Sent: {0}", data);
-                }
+                var rBody = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\nContent-Type: text/plain; charset=UTF-8\n\nHello, world.\n");
+                stream.Write(rBody, 0, rBody.Length);
             }
         }
         catch (SocketException e)
@@ -63,10 +33,7 @@ class MyTcpListener
         }
         finally
         {
-            if (server != null)
-            {
-                server.Stop();
-            }
+            server.Stop();
         }
 
         Console.WriteLine("\nHit enter to continue...");
